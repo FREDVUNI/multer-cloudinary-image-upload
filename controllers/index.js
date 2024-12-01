76166327ad2176160469f1e0d8f632135cee9b48
Fred Vuni <fredvuni809@gmail.com>
2024-12-01 16:07:19 +0300

    chore: clean up and update packages

--- controllers/index.js
+++ controllers/index.js
@@ -1,39 +1,34 @@
-const blog = require("../models/blogSchema") 
+const blog = require("../models/blogSchema");
 
-const HomeController = async(req,res) =>{
-    try{
-        const posts = await blog.find()
-        .sort({createdAt:-1})
-        res.render('../views/index',{posts})
-    }
-    catch(error){
-        console.log(error.message)
-    }
-}
-  
-const PostController = async(req,res) =>{
-    try{
-        res.render('../views/add')
-    }
-    catch(error){
-        console.log(error.message)
-    }
-}
+const HomeController = async (req, res) => {
+  try {
+    const posts = await blog.find().sort({ createdAt: -1 });
+    res.render("../views/index", { posts });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const SinglePostController = async(req,res) =>{
-    try{
-        const post = await blog.findById(req.params.id)
-        
-        if(post){
-            res.render('../views/post',{post})
-        }else{
-            res.redirect("/")
-        }
-    }
-    catch(error){
-        console.log(error.message)
-    }
+const PostController = async (req, res) => {
+  try {
+    res.render("../views/add");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-}
+const SinglePostController = async (req, res) => {
+  try {
+    const post = await blog.findById(req.params.id);
 
-module.exports ={HomeController,PostController,SinglePostController}
+    if (post) {
+      res.render("../views/post", { post });
+    } else {
+      res.redirect("/");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = { HomeController, PostController, SinglePostController };
