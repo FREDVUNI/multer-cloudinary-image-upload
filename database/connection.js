d09d3b2dbee0b529caecb5e960f878f44c73a0f1
Fred Vuni <fredvuni809@gmail.com>
2024-12-01 16:07:25 +0300

    chore: clean up and update packages

--- database/connection.js
+++ database/connection.js
@@ -1,20 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB = async() =>{
-    try{
-        const con = await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser:true, 
-            // useCreateIndex:true,
-            // useFindAndModify:false,
-            // useUnifiedTopology:true  
-        })
-        console.log(`connected to database.`);
-    }
-    catch(error){
-        process.exit(1)
-        console.log(error.message || `Failed to connect to database.`)
-    } 
+const connectDB = async () => {
+  try {
+    const con = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      // useCreateIndex:true,
+      // useFindAndModify:false,
+      // useUnifiedTopology:true
+    });
+    console.log(`connected to database.`);
+  } catch (error) {
+    process.exit(1);
+    console.log(error.message || `Failed to connect to database.`);
+  }
+};
 
-}
-
-module.exports = connectDB
+module.exports = connectDB;
