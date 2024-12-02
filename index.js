06a3ed940272973d20ddde08d01fa77e4c25941e
Fred Vuni <fredvuni809@gmail.com>
2024-12-02 07:45:01 +0300

    chore: update the db connection and clean it up

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ dotenv.config({ path: ".env" });
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
